9c74512ae87320d248c4f7d4072085412793d0bc
Sean Moriarty <spmoriarty1@gmail.com>
2022-05-11 14:20:46 -0700

    restarted the HB, entering in fetch commands and load commands

--- app.js
+++ app.js
@@ -6,3 +6,15 @@ const dogListContainer = document.getElementById('dog-list-container');
 // on load
 // fetch all dogs
 // render and append all dog cards to the container
+async function loadData() {
+    const dogs = await getDogs();
+    console.log(dogs);
+    const main = document.querySelector('main');
+
+    for (let dog of dogs) {
+        const dogDiv = renderDogCard(dog);
+        main.append(dogDiv);
+    }
+}
+
+loadData();
